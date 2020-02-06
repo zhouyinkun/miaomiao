4d5fee99539dccb1b87326dfcd06a637fb8d91fd
zhouyinkun <935548472@qq.com>
2020-02-06 15:15:25 +0800

    add createComponents

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="main">
+        <Header title="我的喵喵"/>
+        <div id="content">
+            <Login />
+        </div>
+        <TabBar />
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import TabBar from '@/components/TabBar';
+//2级路由导入
+import Login from '@/components/Login'
+export default {
+    name : 'Mine',
+    components : {
+        Header,
+        TabBar,
+        Login
+    }
+}
+</script>
+
+<style>
+
+</style>
